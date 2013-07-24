5b50ae81e184a0b34b17655054e33170a6e39f1d
20111144110246 <20111144110246@PAR155976.ifrn.local>
2013-07-24 20:42:43 -0300

    Atualizando o projeto, fazendo o levantamento dos dados para inserir a história do Municipio de Lagoa Salgada.

---
+++ História.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="style02.css" />
+
+<title>História</title>
+</head>
+    <body>
+        <div class="global-div">
+        
+    	
+        
+        <div class="menu-div">
+        	<ul>
+                        <li><a href="#" title="Dados do Municipio">Dados do Municipio</a></li>
+			<li><a href="História.php" title="Como Chegar">Como Chegar</a></li>
+                        <li><a href="gabinete.php" title="Gabinete do Prefeito">Atrações Turisticas</a></li>                                               
+                </ul>
+        </div>
+        
+                <div class="esq-div">
+        
+        	<div class="destaques-div">
+                    <h5>Lagoa Salgada</h5>
+            
+                    <h1>História</h1>
+                    <p class="right17"> ************************************************</p>
+                    <a href="#" title="Leia mais" class="leiamais">leia mais</a>
+                    </div>
+        
+                </div>
+        
+            </div>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="style.css" />
+
+<title>Prefeitura Municipal de Lagoa Salgada</title>
+</head>
+
+<body>
+
+    <div class="global-div">
+        
+    	<div class="topo-div"></div>
+        
+        <div class="menu-div">
+        	<ul>
+				<li><a href="#" title="Página Inicial">Página Inicial</a></li>
+				<li><a href="História.php" title="Lagoa Salgada">Lagoa Salgada</a></li>
+                                <li><a href="gabinete.php" title="Gabinete do Prefeito">Gabinete do Prefeito</a></li>
+				<li><a href="#" title="Secretarias">Secretarias</a></li>
+                                <li><a href="#" title="Serviços">Serviços</a></li>
+                                <li><a href="#" title="Calendário de Pagamentos">Calendário de Pagamentos</a></li>
+                                <li><a href="#" title="Concursos">Concursos</a></li>
+				<li><a href="#" title="Contato">Contato</a></li>                                                
+            </ul>
+        </div>
+        
+        <div class="esq-div">
+        
+        	<div class="destaques-div">
+            <h5>Destaques</h5>
+            
+            <div class="foto-destaque"></div>
+            <h1>Titulo da noticia</h1>
+            <p class="right17"> ************************************************</p>
+            <a href="#" title="Leia mais" class="leiamais">leia mais</a>
+            </div>
+            
+            <div class="chamadas-div">
+            <h4>Galeria de Fotos</h4> 
+            <h2>Titulo da atração</h2> 
+            <p class="right17">*****************************************
+                ************************************
+                ************************************
+                ************************************
+                ************************************</p>            
+            <a href="#" title="Leia mais" class="leiamais">Mais Fotos</a>
+			<h6>Cultura</h6>
+            <ul class="maisartigos">
+            <li><a href="#" title="Carnaval">Carnaval</a></li>
+            <li><a href="#" title="Festa São João">Festa São João</a></li>
+            <li><a href="#" title="Festa São Pedro">Festa São Pedro</a></li>
+            <li><a href="#" title="Festa da Padroeira">Festa da Padroeira</a></li>
+            </ul>      
+            </div>
+            
+            <div class="chamadas-div left17">
+            <h4>Prefeitura Inaugura Escola</h4>
+            <h2>Descrição</h2>
+            <p>*************************************
+                ************************************
+                ************************************
+                ************************************</p>   
+            <a href="#" title="Leia mais" class="leiamais">leia mais</a>
+			<h6>Ultimas noticias</h6>
+            <ul class="maisartigos">
+            <li><a href="#" title="Prefeito Cassado">Prefeito Cassado</a></li>
+            <li><a href="#" title="Reunião na Camara">Reunião na Camara</a></li>
+            <li><a href="#" title="Professores em Greve">Professores em Greve</a></li>
+            <li><a href="#" title="Assalto em Correio">Assalto em Correio</a></li>
+            </ul>      
+                           
+            </div>            
+        
+        </div>
+        <div class="dir-div">
+        
+        <form action="#">
+        <fieldset>
+        <input type="text" name="termo" class="busca" value="Digite o termo e pressione enter" />
+        </fieldset>
+        </form>
+        
+        <div class="maislidos-div">
+            <h4>Mais lidos</h4>
+            <ul class="maisartigos escuro top8">
+            <li><a href="#" title="Prefeito Cassado">Prefeito Cassado</a></li>
+            <li><a href="#" title="Professores do municipio entram em greve">Professores entram em greve</a></li>
+            <li><a href="#" title="Assalto em correio">Assalto em correio</a></li>
+            <li><a href="#" title="Tornei de futsal">Tornei de futsal</a></li>
+            <li><a href="#" title="Festa no Forro da Porteira">Festa no Forro da Porteira</a></li>
+            </ul>
+        </div>
+            <div class="anuncios-div"></div> 
+            <div class="anuncios-div left17"></div> 
+            <div class="anuncios-div "></div> 
+            <div class="anuncios-div left17"></div> 
+        
+        </div>
+        
+        <div class="rodape-div"><p>Rodape</p></div>
+    
+</div>
+</body>
+</html>
